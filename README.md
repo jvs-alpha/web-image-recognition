ee13db117f7bee5054dc1405646a9279043c3354
jvs-alpha <jvsamuel.rufus@gmail.com>
2021-04-07 21:00:51 +0530

    finished validation.py

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # web-image-recognition
 Just for making horse-v-human web accessable
+
+## Base Idea
+In this we are trying to create a web application that can take in a image and tell if its a image of a horse or
+a image of human
+
+## TODO
+1. we need to make the script to work individually
--- validation.py
+++ validation.py
@@ -5,21 +5,28 @@ from PIL import Image
 import numpy as np
 import sys
 
+def loadimage(file):
+    with open(file, "rb") as img:
+        image = Image.open(img).convert("RGB")
+        image = np.asarray(image)
+        # This needs to be in this dimension for model to load it and process it coz we haave trained it with this dimension
+        image = image.reshape((1, 300, 300, 3))
+        return image
+
+
+def imagecheck(model, image):
+    image = loadimage(image)
+    class_names = ['horse', 'human']
+    print(class_names)
+    normalization_layer = keras.layers.experimental.preprocessing.Rescaling(1./255)
+    image_test = normalization_layer(image)
+    model = keras.models.load_model(model)
+    prediction = model.predict(image_test)
+    return class_names[np.argmax(prediction)]
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("Run python hvmtest.py <model> <type>")
         sys.exit(1)
-    test = keras.preprocessing.image_dataset_from_directory(
-        "validation",
-        seed=123,
-        image_size=(300,300),
-    )
-    class_names = ['horses', 'humans']
-    print(class_names)
-    normalization_layer = keras.layers.experimental.preprocessing.Rescaling(1./255)
-    normalized_ds = test.map(lambda x, y: (normalization_layer(x), y))
-    image_test, labels_test = next(iter(normalized_ds))
-    model = keras.models.load_model(sys.argv[1])
-    test_loss, test_acc = model.evaluate(image_test,labels_test)
-    prediction = model.predict(image_test)
+    else:
+        print(imagecheck(sys.argv[1], sys.argv[2]))
